c93afa6afb35ee704832e0906f4dc215a9734cef
tangsect-arch <65212647+tangsect-arch@users.noreply.github.com>
2022-02-07 16:53:29 +0530

    Add files via upload

--- app/v1/routes/customer.js
+++ app/v1/routes/customer.js
@@ -1,11 +1,12 @@
 const router = require('express').Router(),
       customer = require("../controllers/customer");
 
-router.post("/", customer.create);
-router.get("/", customer.findAll);
-router.get("/:id", customer.findOne);
-router.put("/:id", customer.update);
-router.delete("/:id", customer.delete);
-router.put("/changeStatus/:id", customer.changeStatus);
+router.post("/", customer.create)
+      .get("/", customer.findAll);
+
+router.get("/:id", customer.findOne)
+      .put("/:id", customer.update)
+      .delete("/:id", customer.delete)
+      .put("/changeStatus/:id", customer.changeStatus);
 
 module.exports = router;
--- app/v1/routes/employee.js
+++ app/v1/routes/employee.js
@@ -1,11 +1,12 @@
 const router = require('express').Router(),
       employee = require("../controllers/employee");
 
-router.post("/", employee.create);
-router.get("/", employee.findAll);
-router.get("/:id", employee.findOne);
-router.put("/:id", employee.update);
-router.delete("/:id", employee.delete);
-router.put("/changeStatus/:id", employee.changeStatus);
+router.post("/", employee.create)
+      .get("/", employee.findAll);
+
+router.get("/:id", employee.findOne)
+      .put("/:id", employee.update)
+      .delete("/:id", employee.delete)
+      .put("/changeStatus/:id", employee.changeStatus);
 
 module.exports = router;
